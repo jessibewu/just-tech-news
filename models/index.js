65142ba57bc0c61841e18033729cf9cb0abb59ac
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-09-06 14:02:29 -0400

    added comment model/routes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -11,6 +12,7 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
   });
 
+// Votes:
 User.belongsToMany(Post, {
     through: Vote,
     as: 'voted_posts',
@@ -39,4 +41,21 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+// Comments:
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
