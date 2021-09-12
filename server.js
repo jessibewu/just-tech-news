0d28d75ff71524624c8df494c4775fdc5128966c
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-09-12 00:00:04 -0400

    refactor code

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+// add hbs helper functions
+const helpers = require('./utils/helpers');
+
 // require 'path' for static folder below
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// set up Handlebars.js as your app's template engine of choice:
+// set up Handlebars.js as your app's template engine of choice & add helper functions:
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
