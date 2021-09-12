89b6b9542781236b15ed2facb1e06e84ce43e94c
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-09-12 00:06:35 -0400

    corrected homepage hbs

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -7,9 +7,11 @@ test('format_date() returns a date string', () => {
   });
 
 test('format_plural() returns a pluralized word', () => {
-    const plural = format_plural('tiger', 2);
+    const word1 = format_plural('tiger', 1);
+    const word2 = format_plural('lion', 2);
   
-    expect(plural).toBe('tigers');
+    expect(word1).toBe('tiger');
+    expect(word2).toBe('lions');
   });
 
 test('format_url() returns a simplified url string', () => {
